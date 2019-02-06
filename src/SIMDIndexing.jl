7593ecc271f7f02a9a4cb33ebaca23880bd77e52
Takafumi Arakaki <aka.tkf@gmail.com>
2019-02-05 19:14:54 -0800

    Add definitions for MappedArrays

--- src/SIMDIndexing.jl
+++ src/SIMDIndexing.jl
@@ -1,5 +1,17 @@
 module SIMDIndexing
 
-greet() = print("Hello World!")
+using SIMD
+using Requires
+
+const VecOfIntegers{N, T <: SIMD.IntegerTypes} = Vec{N, T}
+
+abstract type AbstractIndex end
+const Index = Union{Int, VecOfIntegers, AbstractIndex}
+
+
+function __init__()
+    @require(MappedArrays="dbb5928d-eab1-5f90-85c2-b9b0edb7c900",
+             include("mappedarrays.jl"))
+end
 
 end # module
